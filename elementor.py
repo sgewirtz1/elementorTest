0b2f47fa9e346415dda00098814fff6618823f94
Simon Gewirtz <sgz@carboard.co>
2021-11-14 22:04:41 +0200

    added file for comments about design and sql question, and updates to task approach

--- elementor.py
+++ elementor.py
@@ -1,6 +1,6 @@
 import requests
 from requests.api import head
-
+from datetime import datetime, timedelta
 
 # read in the request1.csv file line by line
 # for each line (URL) 
@@ -20,35 +20,48 @@ analysis_id_target_prefix = "https://www.virustotal.com/api/v3/analyses/"
 
 url_ids = dict()
 url_ratings = dict()
+url_results = dict()
+url_last_updated_time = dict()
 
 with open("python_workspace/elementorTest/request1.csv") as url_source_file:
     for line in url_source_file:
         url_ids[line.rstrip()] = ""
         url_ratings[line.rstrip()] = dict()
+        url_results[line.rstrip()] = ""
+        url_last_updated_time[line.rstrip()] = None
 
 for url in url_ids.keys():
-    target_url = "url=" + url
-    post_response = requests.request("POST", url_target, data=target_url, headers=post_headers)
+    current_time = datetime.now()
 
-    # capture the id for use in the next query
-    url_id = post_response.json()["data"]["id"]
+    # check that we don't have updated data for url
+    if url_last_updated_time[url] == None or (current_time - url_last_updated_time[url]).total_seconds()/60 > 15:
+        target_url = "url=" + url
+        post_response = requests.request("POST", url_target, data=target_url, headers=post_headers)
 
-    url_ids[url] = url_id
+        # capture the id for use in the next query
+        url_id = post_response.json()["data"]["id"]
 
-    analysis_id_target = analysis_id_target_prefix + url_id
-    print(analysis_id_target)
-    get_headers = {
-        "Accept": "application/json",
-        "x-apikey": api_key
-    }
-    get_response = requests.request("GET", analysis_id_target, headers=get_headers)
-    url_analysis_stats = get_response.json()["data"]["attributes"]["stats"]
+        url_ids[url] = url_id
 
-    url_ratings[url] = url_analysis_stats
+        analysis_id_target = analysis_id_target_prefix + url_id
+
+        get_headers = {
+            "Accept": "application/json",
+            "x-apikey": api_key
+        }
+        get_response = requests.request("GET", analysis_id_target, headers=get_headers)
+        url_analysis_stats = get_response.json()["data"]["attributes"]["stats"]
+
+        url_ratings[url] = url_analysis_stats
+
+        url_last_updated_time[url] = datetime.now()
+
+        if url_analysis_stats["malicious"] > 0:
+            url_results[url] = "risk"
+        else:
+            url_results[url] = "safe"
 
 print(url_ids)
 print(url_ratings)
-
-# SQL Question:
-# SELECT employee_id, MAX(salary) FROM employees GROUP BY department_id
-# SELECT employee_id, salary FROM employees ORDER BY department_id, salary
+print(url_results)
+print(url_last_updated_time)
